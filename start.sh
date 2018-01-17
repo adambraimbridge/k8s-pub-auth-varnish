a2477691d3ada68f10ae1ef70b694dc09108d5cb
David Smith <david.smith@ft.com>
2018-01-17 13:27:37 +0000

    Ignore auth if smartlogic and have an API key (#8)

--- start.sh
+++ start.sh
@@ -15,6 +15,13 @@ log() {
 
 trap 'shutdown' HUP INT QUIT KILL TERM
 
+# Convert environment variables in the conf to fixed entries
+for name in SL_API_KEY
+do
+    eval value=\$$name
+    sed -i "s/$name/${value}/g" /etc/varnish/default.vcl
+done
+
 # Start varnish and log
 log "Starting"
 varnishd -pvcc_allow_inline_c=true -f /etc/varnish/default.vcl -s malloc,1024m -t 5 -p default_grace=0 &
